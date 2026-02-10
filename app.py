1ec485e4d58fb716280731b510164e7c0828f477
Mayank <manku0025@gmail.com>
2026-02-10 22:25:47 +0530

    Remove merge conflict markers from app.py

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ def logout():
 # ---------------- RUN ----------------
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
-=======
+
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import pandas as pd
 import os
